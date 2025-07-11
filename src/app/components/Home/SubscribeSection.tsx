5cdb876ffd85f7d13bb1bcdce067703c2b63947e
RizWaaN3024 <rizwaanans786@gmail.com>
2025-07-12 02:41:50 +0530

    Added input and button from shadcn

---
+++ src/app/components/Home/SubscribeSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SubscribeSection = () => {
+    return (
+        <section className="py-16 lg:py-25">
+            <div className="lg:container mx-auto px-6 lg:px-4">
+                <h2 className="text-xl lg:text-3xl font-bold leading-snug hover:underline hover:text-primary">Subscribe or your code will have bugs 🐛</h2>
+            </div>
+        </section>
+    )
+}
+
+export default SubscribeSection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroGridSection from "./components/Home/HeroGridSection";
+import SubscribeSection from "./components/Home/SubscribeSection";
 
 export default async function Home() {
 
     return (
         <div className="">
             <HeroGridSection />
+            <SubscribeSection />
         </div>
     );
 }
